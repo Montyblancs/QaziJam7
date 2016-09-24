8293c34499b714bf8ac743f04f75261cc26c8479
Steven Osowski <sosowski@comcast.net>
2016-09-23 21:41:12 -0400

    Setting up the basics
    Poops some stuff out.

---
+++ js/mainpage.js
@@ -0,0 +1,11 @@
+//Item as an object : referrs to all interactable entities within rooms : items & people
+	//Properties : Verbs (delimited list or array that can be used on this item)
+	//IsTakeable
+	//Description : What to display when examined
+	//IsCombineable : Can you mash this with something else to make a new item?
+	//ChatDiolauge : If 'talk' is an accepted verb, write diolauge here. (delimited string or array?)
+
+//Rooms : array of all available rooms
+	//Description : What player sees on entry/examine
+	//Exits : N S W E
+
